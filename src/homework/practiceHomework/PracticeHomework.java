0720889e8d436baa3c92ef6566dfb041a80bc305
davsimonyan <davosim577@gmail.com>
2022-06-02 21:44:51 +0400

    PracticeHomework

--- src/homework/practiceHomework/PracticeHomework.java
+++ src/homework/practiceHomework/PracticeHomework.java
@@ -15,31 +15,30 @@ public class PracticeHomework {
     }
 
     boolean isSameNum(int a, int b) {
-        if (a == b)
+        if (a == b) {
             return true;
-
+        }
         return false;
     }
 
     boolean lessThanOrEqualToZero(int number) {
-        if (number <= 0)
+        if (number <= 0) {
             return true;
-
+        }
         return false;
     }
 
     boolean reverseBool(boolean value) {
-        if (value == false)
+        if (value == false) {
             return true;
-
+        }
         return false;
     }
 
     int maxLength(int[] array1, int[] array2) {
-        if (array1.length > array2.length)
+        if (array1.length > array2.length) {
             return array1.length;
-
-        else return array2.length;
+        } else return array2.length;
 
     }
 
